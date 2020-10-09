f04c5658a912baac73dd1a3ed79996f4dcef6a8d
RichardGaillet <gaillet.richard@gmail.com>
2020-10-09 13:28:32 +0200

    Add signin and signup buttons.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,11 +8,34 @@ export default function HomeScreen() {
                 <Image source={require('../assets/logo_aos.png')} />
                 <Text>Allez, On Sort !</Text>
             </View>
+            <View style={styles.buttonGroup}>
+                <View style={styles.button}>
+                    <Button
+                        accessibilityLabel={'Se connecter'}
+                        onPress={() => { alert('Se connecter') }}
+                        title={'Se connecter'}
+                    />
+                </View>
+                <View style={styles.button}>
+                    <Button
+                        accessibilityLabel={'S\'inscrire'}
+                        onPress={() => { alert('S\'inscrire') }}
+                        title={'S\'inscrire'}
+                    />
+                </View>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    button: {
+        padding: 8,
+    },
+    buttonGroup: {
+        flexDirection: 'row',
+        padding: 8,
+    },
     container: {
         alignItems: 'center',
         flex: 1,
