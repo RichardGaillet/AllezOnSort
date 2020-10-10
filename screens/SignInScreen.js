54ed9b13cb28f173359507ab1dc96c53d0587038
RichardGaillet <gaillet.richard@gmail.com>
2020-10-10 10:49:50 +0200

    Init sign in screen.

---
+++ screens/SignInScreen.js
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from 'react'
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+
+export default function SignInScreen() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [signInDisabled, setSignInDisabled] = useState(true);
+
+    useEffect(() => {
+        if (email.trim().length > 0 && password.trim().length > 0) {
+            setSignInDisabled(false)
+        } else {
+            setSignInDisabled(true)
+        }
+    }, [email, password])
+
+    const submitLogin = () => {
+        setPassword('')
+    }
+
+    const CustomButton = ({ onPress, title }) => (
+        <TouchableOpacity
+            accessibilityLabel={`Bouton - ${title}`}
+            accessibilityHint="Entrer l'email et le mot de passe avant de valider"
+            onPress={onPress}
+            style={signInDisabled ? styles.customButtonContainerDiabled : styles.customButtonContainer}
+        >
+            <Text style={signInDisabled ? styles.customButtonTextDisabled : styles.customButtonText}>{title}</Text>
+        </TouchableOpacity>
+    );
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.logoCatchPhrase}>
+                <Image
+                    accessibilityLabel={'Logo AOS'}
+                    source={require('../assets/logo_aos.png')}
+                />
+                <Text>Allez, On Sort !</Text>
+            </View>
+            <View>
+                <View style={styles.textInputBox}>
+                    <Text>Adresse email</Text>
+                    <View style={styles.textInputField}>
+                        <TextInput
+                            autoCapitalize={'none'}
+                            autoCompleteType={'email'}
+                            // autoFocus
+                            blurOnSubmit
+                            color={'#fff'}
+                            keyboardType={'email-address'}
+                            maxLength={64}
+                            onChangeText={email => setEmail(email)}
+                            placeholder={'exemple.adresse@email.com'}
+                            placeholderTextColor={'#dfdfdf'}
+                            returnKeyType="next"
+                            selectionColor={'#fff'}
+                            spellCheck={false}
+                            textContentType={'emailAddress'}
+                            value={email} />
+                    </View>
+                </View>
+                <View style={styles.textInputBox}>
+                    <Text>Mot de Passe</Text>
+                    <View style={styles.textInputField}>
+                        <TextInput
+                            color={'#fff'}
+                            maxLength={32}
+                            onChangeText={password => setPassword(password)}
+                            placeholder={'●●●●●●●●●●'}
+                            placeholderTextColor={'#dfdfdf'}
+                            returnKeyType="next"
+                            secureTextEntry
+                            spellCheck={false}
+                            value={password} />
+                    </View>
+                </View>
+                <View style={styles.button}>
+                    <CustomButton
+                        onPress={() => { submitLogin() }}
+                        title={'Se connecter'}
+                    />
+                </View>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        padding: 8,
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'space-around',
+    },
+    customButtonContainer: {
+        elevation: 4,
+        backgroundColor: "#fb483e",
+        borderRadius: 2,
+        paddingVertical: 8,
+    },
+    customButtonContainerDiabled: {
+        backgroundColor: "#dfdfdf",
+        borderRadius: 2,
+        paddingVertical: 8,
+    },
+    customButtonText: {
+        color: "#fff",
+        alignSelf: "center",
+        textTransform: "uppercase"
+    },
+    customButtonTextDisabled: {
+        color: "#a2a2a2",
+        alignSelf: "center",
+        textTransform: "uppercase"
+    },
+    logoCatchPhrase: {
+        alignItems: 'center',
+    },
+    textInputField: {
+        backgroundColor: '#9000ae',
+        borderBottomWidth: 2,
+        borderRadius: 2,
+        borderColor: '#fb483e',
+        color: '#fff',
+        padding: 8,
+    },
+    textInputBox: {
+        backgroundColor: 'rgba(255, 255, 255, 0.75)',
+        padding: 8,
+    },
+})
