296f52ad20de6ff184754c83144b9868665d770d
RichardGaillet <gaillet.richard@gmail.com>
2020-10-11 21:05:17 +0200

    Add link to activities & activity screens in navigator stack.

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
 import SignUpScreen from './screens/SignUpScreen';
 import SignInScreen from './screens/SignInScreen';
+import ActivitiesScreen from './screens/ActivitiesScreen';
+import ActivityScreen from './screens/ActivityScreen';
 import colors from './config/colors'
 
 const Stack = createStackNavigator();
@@ -28,6 +30,8 @@ export default function App() {
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Plus une seconde à perdre ! ⏱️" }} />
         <Stack.Screen name="SignIn" component={SignInScreen} options={{ title: "Se connecter" }} />
         <Stack.Screen name="SignUp" component={SignUpScreen} options={{ title: "S'inscrire" }} />
+        <Stack.Screen name="Activities" component={ActivitiesScreen} options={{ title: "Activités" }} />
+        <Stack.Screen name="Activity" component={ActivityScreen} options={{ title: "Activité" }} />
       </Stack.Navigator>
     </NavigationContainer >
   );
