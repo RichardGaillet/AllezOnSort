ada13bf14d3015a8533bffbb1a33e23ec42da86e
RichardGaillet <gaillet.richard@gmail.com>
2020-10-13 08:37:05 +0200

    Init Members screen.

---
+++ screens/MembersScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function MembersScreen() {
+    return (
+        <View>
+            <Text>Members</Text>
+        </View>
+    )
+}
