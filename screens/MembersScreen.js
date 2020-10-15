93eb7dcb139662efd097ebd28dcdf3df75df62f8
RichardGaillet <gaillet.richard@gmail.com>
2020-10-15 12:47:35 +0200

    Switch member sheet from Dailog to Card element.

--- screens/MembersScreen.js
+++ screens/MembersScreen.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import { AsyncStorage, Button, StyleSheet, View } from 'react-native';
-import { Avatar, Dialog, FAB, Paragraph, Portal, Provider } from 'react-native-paper'
+import { AsyncStorage, StyleSheet } from 'react-native';
+import { Button, Card, Dialog, FAB, Portal, Provider, Text, Divider } from 'react-native-paper'
 import { ScrollView } from 'react-native-gesture-handler'
 import MasonryList from "react-native-masonry-list"
 import members from "../mocks/members.json"
@@ -55,10 +55,10 @@ export default function MembersScreen() {
         return (
             <Portal>
                 <FAB.Group
-                    accessibilityLabel="Modifier le nombre de colonnes"
+                    accessibilityLabel={`Modifier le nombre de colonnes de 1 à 5, il y a ${masonryListColumns} colonnes actuellement`}
                     fabStyle={{ backgroundColor: colors.secondary }}
                     open={open}
-                    icon={open ? `chevron-up` : 'format-columns'}
+                    icon={open ? `chevron-up` : 'view-dashboard-variant'}
                     actions={[
                         {
                             accessibilityLabel: "Affichage sur une colonne",
@@ -100,32 +100,50 @@ export default function MembersScreen() {
     const [visible, setVisible] = useState(false);
     const [member, setMember] = useState({})
     const showDialog = (member) => {
-        setMember(member)
+        setMember(member);
         setVisible(true);
     }
     const hideDialog = () => setVisible(false);
     const memberDialog = () => {
+        const { description, lastConnection, personalInformations, username } = member
         return (
             <Portal>
                 <Dialog visible={visible} onDismiss={hideDialog}>
                     <ScrollView>
-                        <View style={styles.dialogTitleBox}>
-                            <Avatar.Image style={styles.avatarImage} source={{ uri: member?.personalInformations?.photo }} />
-                            <Dialog.Title>{member?.username || 'Descriptif'}</Dialog.Title>
-                        </View>
-                        <Dialog.Content>
-                            <Paragraph>{JSON.stringify(member)}</Paragraph>
-                        </Dialog.Content>
-                        <Dialog.Actions>
-                            <Button
-                                color={colors.secondary}
-                                onPress={hideDialog}
-                                title='Fermer'
-                            />
-                        </Dialog.Actions>
+                        <Card>
+                            <Card.Title title={username} subtitle={`Dernière connexion : ${lastConnection}`} titleStyle={styles.cardTitle} subtitleStyle={styles.cardTitle} style={styles.cardTitle} />
+                            <Card.Cover source={{ uri: personalInformations?.photo }} />
+                            <Divider />
+                            <Card.Content>
+                                {personalInformations?.firstname && <>
+                                    <Text style={styles.cardContentText} >Prénom : {personalInformations?.firstname}</Text>
+                                </>}
+                                {personalInformations?.birthday && <>
+                                    <Divider />
+                                    <Text style={styles.cardContentText} >Date de naissance : {personalInformations?.birthday}</Text>
+                                </>}
+                                {personalInformations?.location && <>
+                                    <Divider />
+                                    <Text style={styles.cardContentText} >Localisation : {personalInformations?.location}</Text>
+                                </>}
+                                {description && <>
+                                    <Divider />
+                                    <Text style={styles.cardContentText} >Description : {description}</Text>
+                                </>}
+                            </Card.Content>
+                            <Card.Actions>
+                                <Button
+                                    color={colors.secondary}
+                                    icon='close-circle'
+                                    mode="contained"
+                                    onPress={hideDialog}
+                                    style={{ elevation: 4 }}
+                                >Fermer</Button>
+                            </Card.Actions>
+                        </Card>
                     </ScrollView>
                 </Dialog>
-            </Portal>
+            </Portal >
         )
     }
 
@@ -139,20 +157,11 @@ export default function MembersScreen() {
 }
 
 const styles = StyleSheet.create({
-    avatarImage: {
-        backgroundColor: colors.primary,
-        justifyContent: 'center',
+    cardContentText: {
+        marginVertical: 12
     },
-    dialogTitle: {
-        flex: 0.5,
-        justifyContent: 'center',
+    cardTitle: {
+        backgroundColor: colors.secondary,
+        color: '#fff',
     },
-    dialogTitleBox: {
-        flexDirection: 'row',
-        padding: 8,
-    },
-    divider: {
-        borderColor: colors.secondary,
-        borderBottomWidth: 2,
-    }
 })
