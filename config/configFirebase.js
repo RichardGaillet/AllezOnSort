fae6a3a9c17d54dd691287aaac6075483fe755e9
RichardGaillet <gaillet.richard@gmail.com>
2020-10-21 09:55:06 +0200

    Add config file.

---
+++ config/configFirebase.js
@@ -0,0 +1,21 @@
+import {
+    API_KEY,
+    API_AUTH_DOMAIN,
+    API_DATABASE_URL,
+    API_PROJECT_ID,
+    API_STORAGE_BUCKET,
+    API_MESSAGING_SENDER_ID,
+    API_APP_ID,
+    API_MEASUREMENT_ID
+} from '@env'
+
+export default firebaseConfig = {
+    apiKey: API_KEY,
+    authDomain: API_AUTH_DOMAIN,
+    databaseURL: API_DATABASE_URL,
+    projectId: API_PROJECT_ID,
+    storageBucket: API_STORAGE_BUCKET,
+    messagingSenderId: API_MESSAGING_SENDER_ID,
+    appId: API_APP_ID,
+    measurementId: API_MEASUREMENT_ID
+};
