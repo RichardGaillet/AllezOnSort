e77defcbab605c79a0e3f617257a21ac3a560dc4
RichardGaillet <gaillet.richard@gmail.com>
2020-10-23 00:06:09 +0200

    Modify header title on navigation bar.

--- App.js
+++ App.js
@@ -38,7 +38,7 @@ export default function App() {
         <Stack.Screen name="SignUp" component={SignUpScreen} options={{ title: "S'inscrire" }} />
         <Stack.Screen name="Activities" component={ActivitiesScreen} options={{ title: "Activités" }} />
         <Stack.Screen name="Activity" component={ActivityScreen} options={{ title: "Activité" }} />
-        <Stack.Screen name="NewActivity" component={NewActivityScreen} options={{ title: "Ajouter une nouvelle activité" }} />
+        <Stack.Screen name="NewActivity" component={NewActivityScreen} options={{ title: "Ajouter une activité" }} />
         <Stack.Screen name="Members" component={MembersScreen} options={{ title: "Membres" }} />
       </Stack.Navigator>
     </NavigationContainer >
