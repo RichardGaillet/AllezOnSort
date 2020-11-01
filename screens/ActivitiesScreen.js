20e1cac95aa94d3b06d1ef73e5d49852957a1206
RichardGaillet <gaillet.richard@gmail.com>
2020-11-01 16:55:41 +0100

    In case of failure.

--- screens/ActivitiesScreen.js
+++ screens/ActivitiesScreen.js
@@ -25,6 +25,10 @@ export default function ActivitiesScreen({ navigation }) {
                 setActivities(Object.values(snapshot.val()))
                 setLoading(false)
             })
+            .catch(() => {
+                setActivities(null)
+                setLoading(false)
+            })
     }, [])
 
     const futureActivitiesArray = () => {
