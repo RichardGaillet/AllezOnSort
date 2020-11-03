0c17d2fb8ff01ba2ef67a005900caac2b4f0caeb
RichardGaillet <gaillet.richard@gmail.com>
2020-11-03 18:22:01 +0100

    Import firebase.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Image, StatusBar, StyleSheet, Text, View } from 'react-native'
 import { Button } from 'react-native-paper'
 import colors from '../config/colors'
+
+import * as firebase from 'firebase'
 
 export default function HomeScreen({ navigation }) {
     return (
