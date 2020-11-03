1430dd73c80707eaad4d6cde987e4b8b83f170e3
RichardGaillet <gaillet.richard@gmail.com>
2020-11-03 10:26:04 +0100

    Add Scrollview.

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
 import colors from '../config/colors'
 
 import * as firebase from 'firebase';
@@ -36,51 +36,53 @@ export default function SignInScreen({ navigation }) {
 
     return (
         <View style={styles.container}>
-            <View>
-                <View style={styles.textInputBox}>
-                    <Text>Adresse email</Text>
-                    <View style={styles.textInputField}>
-                        <TextInput
-                            autoCapitalize={'none'}
-                            autoCompleteType={'email'}
-                            blurOnSubmit
-                            color={colors.light}
-                            keyboardType={'email-address'}
-                            maxLength={64}
-                            onChangeText={email => setEmail(email)}
-                            placeholder={'exemple.adresse@email.com'}
-                            placeholderTextColor={colors.placeholder}
-                            returnKeyType="next"
-                            selectionColor={colors.light}
-                            spellCheck={false}
-                            textContentType={'emailAddress'}
-                            value={email} />
+            <ScrollView>
+                <View>
+                    <View style={styles.textInputBox}>
+                        <Text>Adresse email</Text>
+                        <View style={styles.textInputField}>
+                            <TextInput
+                                autoCapitalize={'none'}
+                                autoCompleteType={'email'}
+                                blurOnSubmit
+                                color={colors.light}
+                                keyboardType={'email-address'}
+                                maxLength={64}
+                                onChangeText={email => setEmail(email)}
+                                placeholder={'exemple.adresse@email.com'}
+                                placeholderTextColor={colors.placeholder}
+                                returnKeyType="next"
+                                selectionColor={colors.light}
+                                spellCheck={false}
+                                textContentType={'emailAddress'}
+                                value={email} />
+                        </View>
+                    </View>
+                    <View style={styles.textInputBox}>
+                        <Text>Mot de passe</Text>
+                        <View style={styles.textInputField}>
+                            <TextInput
+                                color={colors.light}
+                                maxLength={32}
+                                onChangeText={password => setPassword(password)}
+                                placeholder={'●●●●●●●●●●'}
+                                placeholderTextColor={colors.placeholder}
+                                returnKeyType="next"
+                                secureTextEntry
+                                spellCheck={false}
+                                value={password} />
+                        </View>
+                    </View>
+                    <View style={styles.button}>
+                        <Button
+                            color={colors.secondary}
+                            disabled={signInDisabled}
+                            onPress={signIn}
+                            title={'Se connecter'}
+                        />
                     </View>
                 </View>
-                <View style={styles.textInputBox}>
-                    <Text>Mot de passe</Text>
-                    <View style={styles.textInputField}>
-                        <TextInput
-                            color={colors.light}
-                            maxLength={32}
-                            onChangeText={password => setPassword(password)}
-                            placeholder={'●●●●●●●●●●'}
-                            placeholderTextColor={colors.placeholder}
-                            returnKeyType="next"
-                            secureTextEntry
-                            spellCheck={false}
-                            value={password} />
-                    </View>
-                </View>
-                <View style={styles.button}>
-                    <Button
-                        color={colors.secondary}
-                        disabled={signInDisabled}
-                        onPress={signIn}
-                        title={'Se connecter'}
-                    />
-                </View>
-            </View>
+            </ScrollView>
         </View>
     )
 }
