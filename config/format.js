9c5c682d3565f5ee49ad11fc422849070c57c338
RichardGaillet <gaillet.richard@gmail.com>
2020-11-04 03:39:56 +0100

    Put conditional chaining.

--- config/format.js
+++ config/format.js
@@ -7,7 +7,7 @@ export const lessThanTen = (number) => {
 }
 
 export const shortenText = (text, limit) => {
-    if (text.length > limit) {
+    if (text?.length > limit) {
         return text.substring(0, (limit - 3)) + '...'
     }
     return text
